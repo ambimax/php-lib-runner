261c2b5929f4fe1a6ec2d0ae9bd16490d27b0224
fabiankohnen <fk@ambimax.de>
2022-12-15 11:01:36 +0100

    fix: cs-fix

--- src/ArgumentBag/ArgumentValidationException.php
+++ src/ArgumentBag/ArgumentValidationException.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Ambimax\Runner\ArgumentBag;
 
-use Exception;
-use Throwable;
-
-class ArgumentValidationException extends Exception
+class ArgumentValidationException extends \Exception
 {
-    public function __construct(string $argument, string $message, int $code = 0, Throwable $previous = null)
+    public function __construct(string $argument, string $message, int $code = 0, \Throwable $previous = null)
     {
         $message = 'Error while validation of '.$argument.': '.$message;
         parent::__construct($message, $code, $previous);
--- src/ArgumentBag/MultipleArgumentValidationException.php
+++ src/ArgumentBag/MultipleArgumentValidationException.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace Ambimax\Runner\ArgumentBag;
 
-use Exception;
-use Throwable;
-
-class MultipleArgumentValidationException extends Exception
+class MultipleArgumentValidationException extends \Exception
 {
     /**
      * @param ArgumentValidationException[] $exceptions
      */
-    public function __construct(array $exceptions, int $code = 0, Throwable $previous = null)
+    public function __construct(array $exceptions, int $code = 0, \Throwable $previous = null)
     {
         $message = "One or more exceptions occurred during argument validation:\n";
         foreach ($exceptions as $exception) {
